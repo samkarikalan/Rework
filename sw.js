0f91819a40c4be547eafceab389f99705e64f80b
samkarikalan <karikalan.indo@gmail.com>
2026-04-04 17:55:00 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Caches app shell for offline use
    ============================================= */
 
-const CACHE_NAME = 'club-scheduler-v86';
+const CACHE_NAME = 'club-scheduler-v87';
 
 const ASSETS = [
   './KariBRRApp.html',
